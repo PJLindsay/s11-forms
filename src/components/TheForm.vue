c22f06e4fd70fbf59c677f7196754f890c6f11bc
PJ Lindsay <pjlindsay@gmail.com>
2020-10-09 15:20:13 -0600

    How to use v-model with custom control

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -49,7 +49,7 @@
       </div>
     </div>
     <div class="form-control">
-      <rating-control></rating-control>
+      <rating-control v-model="rating"></rating-control>
     </div>
     <div class="form-control">
       <input type="checkbox" id="confirm-terms" name="confirm-terms" v-model="confirm"/>
@@ -78,6 +78,7 @@ export default {
       interest: [],
       how: null,
       confirm: false,
+      rating: null,
       userNameValidity: 'pending'
     }
   },
@@ -99,6 +100,8 @@ export default {
       this.how = null
       console.log('Confirm: ', this.confirm)
       this.confirm = false
+      console.log('Rating: ', this.rating)
+      this.rating = null
     },
     validateInput() {
       if (this.userName === '') {
